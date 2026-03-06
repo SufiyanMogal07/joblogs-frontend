2ad831a8757bbcdb07d6a547b24f18c3f109b8f2
Sufiyan Mogal <sufiyanyaseenmogal@gmail.com>
2026-03-06 23:50:13 +0530

    feat(auth) : added zod schema and extract types for registration and login

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,5 @@
+import z from "zod";
+
 export type JobStatus =
   | "draft"
   | "applied"
@@ -5,6 +7,24 @@ export type JobStatus =
   | "offer"
   | "rejected" 
   | "";
+
+export type responseType = {
+  success: boolean,
+  message: string,
+  data?: unknown
+}
+
+const baseAuthObject = z.object({
+  name: z.string().trim().min(3,"Full name is required"),
+  email: z.string().trim().email({message: "Email is invalid!"}),
+  password : z.string().min(6,"Password should be atleast of 6 characters").max(10,"Password maximum (10) characters reached")
+});
+
+export const registerSchema = baseAuthObject.extend({});
+export const loginSchema = baseAuthObject.omit({name: true});
+
+export type registerValues = z.infer<typeof registerSchema>;
+export type loginValues = z.infer<typeof loginSchema>;
 
 export type JobApplication = {
   id: string;
