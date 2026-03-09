47ec2e1d538e4587fcf73c4315d84853e7845854
Sufiyan Mogal <sufiyanyaseenmogal@gmail.com>
2026-03-09 11:28:06 +0530

    refactor: update auth service for login and register - add return type for response data - minimize the code and used axios interceptor

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,14 +1,10 @@
 import { axiosInstance } from "@/lib/axios";
-import { loginValues, registerValues } from "@/types";
+import { ApiResponse, loginValues, registerValues } from "@/types";
 
-export const authRegister = async (data: registerValues) => {
-  const result = await axiosInstance.post("/auth/register", data);
-
-  return result.data;
+export const authRegister = async (data: registerValues) : ApiResponse  => {
+  return await axiosInstance.post("/auth/register", data);
 };
 
-export const authLogin = async (data: loginValues) => {
-  const result = await axiosInstance.post("/auth/login", data);
-
-  return result.data;
+export const authLogin = async (data: loginValues) : ApiResponse => {
+  return await axiosInstance.post("/auth/login", data);
 };
