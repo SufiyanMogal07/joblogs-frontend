ef676b44456f9ee016b0c1104acc8a0f93772def
Sufiyan Mogal <sufiyanyaseenmogal@gmail.com>
2026-03-09 10:53:11 +0530

    feat: added toast component in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased bg-slate-950 text-white md:px-8 max-w-400 mx-auto`}
       >
         {children}
+        <Toaster richColors position="top-right"/>
       </body>
     </html>
   );
